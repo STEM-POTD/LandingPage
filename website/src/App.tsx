e8e5490fc9ae626cff1a516c38c0646e757422db
SachetK <71898487+SachetK@users.noreply.github.com>
2022-11-20 14:46:38 -0500

    Switch to React Router

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,46 +1,26 @@
 import React, { useState } from 'react'
-import LandingMainComponent from './components/landing/LandingMainComponent'
 import HeaderComponent from './components/HeaderComponent'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import FooterComponent from 'components/FooterComponent'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { trpc } from 'utils/trpc'
 import { httpBatchLink } from '@trpc/client'
-import { createReactRouter, createRouteConfig, Outlet, RouterProvider } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LandingMainComponent from 'components/landing/LandingMainComponent'
 import PracticeMainComponent from 'components/practice/PracticeMainComponent'
-
 const queryClient = new QueryClient()
 
-const rootRoute = createRouteConfig()
-
-const indexRoute = rootRoute.createRoute({
-  path: '/',
-  component: LandingMainComponent,
-})
-
-const practiceProblemsRoute = rootRoute.createRoute({
-  path: '/practice',
-  component: PracticeMainComponent,
-})
-
-const routeConfig = rootRoute.addChildren([
-  indexRoute,
-  practiceProblemsRoute,
-])
-
-// Set up a ReactRouter instance
-const router = createReactRouter({
-  routeConfig,
-  defaultPreload: 'intent',
-})
-
-declare module '@tanstack/react-router' {
-  interface RegisterRouter {
-    router: typeof router
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingMainComponent />,
+  },
+  {
+    path: '/practice',
+    element: <PracticeMainComponent />,
   }
-}
+])
 
 function App() {
     const [trpcClient] = useState(() => trpc.createClient({
@@ -55,23 +35,20 @@ function App() {
         <>
             <trpc.Provider client={trpcClient} queryClient={queryClient}>
                 <QueryClientProvider client={queryClient}>
-                    <RouterProvider router={router}>
-                        <HeaderComponent />
-                        <Outlet /> {/* Start rendering router matches */}
-                        <TanStackRouterDevtools position="bottom-left" />
-                        <FooterComponent />
-                    </RouterProvider>
+                    <HeaderComponent />
+                    <RouterProvider router={router} />
                     <ReactQueryDevtools
-                    initialIsOpen
-                    position="bottom-left"
-                    toggleButtonProps={{
-                    style: {
-                        marginLeft: '5.5rem',
-                        transform: `scale(.7)`,
-                        transformOrigin: 'bottom left',
-                    },
-                    }}
-                />
+                      initialIsOpen
+                      position="bottom-left"
+                      toggleButtonProps={{
+                      style: {
+                          marginLeft: '5.5rem',
+                          transform: `scale(.7)`,
+                          transformOrigin: 'bottom left',
+                      },
+                      }}
+                    />
+                    <FooterComponent />
                 </QueryClientProvider>
             </trpc.Provider>
         </>
